a31d9f081a1963321b1ee7667e7e393e3a1dc89c
Bruno <bruno.oliveira360@gmail.com>
2019-10-13 17:43:53 +0100

    Exemplo de programa para o exercício 2 da ficha prática 1

---
+++ MinutosParaSegundos - FP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Declaração de variáveis
+            double minutos, segundos;
+            string minutosString;
+
+            //Leitura do teclado
+            Console.WriteLine("Introduza os minutos:");
+            minutosString = Console.ReadLine();
+
+            //conversão da string lida do teclado para double
+            minutos = double.Parse(minutosString);
+
+            //cálculo dos segundos
+            segundos = minutos * 60;
+
+            //imprimir resultados
+            Console.WriteLine("Resultado: "+ segundos);
+
+
+			/**
+			 * Instrução facultativa que faz com que o programa apenas termine
+			 * após a introdução de algum valor
+			 */
+			Console.ReadKey();
+		}
+    }
+
