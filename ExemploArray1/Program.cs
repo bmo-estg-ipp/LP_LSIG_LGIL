2878bdf5c0098e6cfa32da938f24eb4faa7e629e
Bruno <bruno.oliveira360@gmail.com>
2019-12-05 16:58:37 +0000

    Exemplos Array

---
+++ ExemploArray1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/**
+ *  Exemplo de programa que declara um array,
+ *  atribui um conjunto de 4 valores ao array e imprime-os
+ *  de seguida
+ *
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Declaração da constante para o número de elementos
+        const int TAMANHO = 5;
+        /**Declaração do array, seguida de uma inicialização
+        * de cada um dos seus elementos
+        */
+        int[] numeros = new int[TAMANHO];
+        numeros[0] = 1;
+        numeros[1] = 2;
+        numeros[2] = 3;
+        numeros[3] = 4;
+        numeros[4] = 5;
+
+        //output do contéudo do array
+        for(int i = 0;i < TAMANHO; i++)
+        {
+            Console.WriteLine(numeros[i]);
+        }
+    }
+}
+
---
+++ ExemploArray2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/**
+ *  Exemplo de programa que declara um array,
+ *  lê um conjunto de 10 valores para um array e imprime-os
+ *  de seguida
+ *
+ */
+
+namespace ExemploArrays2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração da constante para o número de elementos
+            const int TAMANHO = 10;
+
+            //Declaração do array
+            int[] numeros = new int[TAMANHO];
+
+
+            //input para o array
+            for (int i = 0; i < TAMANHO; i++)
+            {
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            //output
+            for (int i = 0; i < TAMANHO; i++)
+            {
+                Console.WriteLine(numeros[i]);
+            }
+        }
+    }
+}
