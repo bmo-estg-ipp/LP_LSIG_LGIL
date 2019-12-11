5dd900024f864cc1c2620e9bab7d499d878d4199
Bruno <bruno.oliveira360@gmail.com>
2019-12-11 12:21:18 +0000

    Exemplos com métodos

---
+++ ExemploArray2ComMetodos/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ExemploArrays2ComMetodos
+{
+    /**
+     *  Exemplo tendo por base o exemplo: ExemploArrays2 com a utilização
+     *  de métodos.
+     *  Neste exemplo, foi adicionado um novo array para exemplificar
+     *  as potencialidades de reutilização de código que os métodos
+     *  disponibilizam.
+     *
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			//Declaração da constante para o número de elementos
+			const int TAMANHO = 5;
+
+			//Declaração dos arrays com o mesmo tamanho
+			int[] numeros = new int[TAMANHO];
+            int[] numeros2 = new int[TAMANHO];
+
+            /**
+             * Ambos os métodos criados para este exemplo, não têm retorno (void)
+             * Quando utilizamos um array como parâmetro de um método estamos
+             * a fazer uma passagem por referência. Isto significa que se alterarmos
+             * o array dentro do método, essa alteração irá ser refletida em todos
+             * os outros métodos onde o array será utilizado.
+             */
+
+            //invocação do método para leitura dos arrays
+            lerArray(numeros, TAMANHO);
+            lerArray(numeros2, TAMANHO);
+
+            //invocação do método para imprimir os arrays
+            imprimirArray(numeros, TAMANHO);
+            imprimirArray(numeros2, TAMANHO);
+
+        }
+
+        /**
+         * Os métodos possuem parâmetros que identificam as regras de
+         * como esse método deverá ser utilizado. De notar que os parâmetros
+         * podem ter o mesmo nome dos argumentos que são utilizados na invocação
+         * do método. Neste exemplo, os argumentos e parâmetros têm nomes diferentes.
+         * Os arrays: numeros e numeros2 são identificados internamente em cada um
+         * dos métodos como arrayNumeros (termo mais genérico para referenciar qualquer
+         * array enviado como argumento. Os parâmetros e variáveis de cada método
+         * só existem no contexto do método, não sendo visíveis para os restantes
+         * métodos do programa.
+         * Tem que existir correspondência de tipos entre os parâmetros
+         * e argumentos (int[] e int, respetivamente).
+         * void significa que o método não tem retorno (a referência com o nome:
+         * arrayNumeros altera diretamente os valores associados aos arrays enviados
+         * por argumento (numeros e numeros2 - independentes de cada invocação do
+         * método)
+         * 
+         */
+        static void lerArray(int[] arrayNumeros, int numeroElementos)
+        {
+            //input para o array
+            for (int i = 0; i < numeroElementos; i++)
+            {
+                arrayNumeros[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void imprimirArray(int[] arrayNumeros, int numeroElementos)
+        {
+            //input para o array
+            for (int i = 0; i < numeroElementos; i++)
+            {
+                Console.WriteLine(arrayNumeros[i]);
+            }
+        }
+    }
+}
---
+++ ExemploMetodos/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ExemploMetodos
+{
+    /**
+     * Neste exemplo pretende-se ler a altura e comprimento de uma janela
+     * tendo por base as seguintes restrições:
+     * valor da altura entre 0.5 e 5
+     * valor do comprimento entre 1 e 6
+     * No final imprime a área da janela
+     *
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de constantes para os limites
+            const double MIN_ALTURA = 0.5, MAX_ALTURA = 5, MIN_COMP = 1, MAX_COMP = 6;
+            //Declaração de variáveis
+            double altura, comprimento, area;
+
+            //ler os valores utilizando o método lerValor
+            altura = LerValor(MIN_ALTURA, MAX_ALTURA);
+            comprimento = LerValor(MIN_COMP, MAX_COMP);
+
+            //apresentar cálculo
+            area = comprimento * altura;
+
+            Console.WriteLine("A área é: " + area);
+        }
+
+        /**
+         * Caso os valores não estejam nos limites estipulados,
+         * o retorno assume o mínimo caso seja inferior a esse mínimo
+         * e assume o máximo caso seja superior a esse máximo
+         *
+         * Neste exemplo, o método retorna um valor do tipo double.
+         * Nas variáveis do tipo primitivo sé realizada uma passagem por valor.
+         * Isto é, é sempre enviada uma cópia do valor enviado como argumento,
+         * o que significa que alterações aos valores
+         * dos parâmetros (dentro do método) não têm impacto no valor dos argumentos
+         * enviados por outros métodos na invocação do método.
+         */
+        static double LerValor(double min, double max)
+        {
+            double valor;
+            valor = double.Parse(Console.ReadLine());
+            if(valor < min)
+            {
+                valor = min;
+            }
+            if(valor > max)
+            {
+                valor = max;
+            }
+
+            return valor;
+        }
+    }
+}
